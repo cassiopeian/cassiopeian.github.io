72921ef7992534ccbfe4cb446d96997de69d185b
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-06-11 18:39:35 -0400

    update window on scroll to check element lengths
    Added some if statements that check whether an element exists on the page, so I could adjust the header color on the illustration page, too. (Otherwise, an error comes up saying top is undefined.)

--- js/main.js
+++ js/main.js
@@ -78,26 +78,8 @@ $('#see-about, #see-projects').on('click', function() {
 });
 
 $(window).on('scroll', function() {
-    let aboutTop = $('#about').offset().top - $(window).scrollTop();
-    let vanishingPoint = $('#hero').height() * .75;
     let headerHeight = $('header').height();
-    let projectsTop = Math.floor($('#projects').offset().top) - $(window).scrollTop();
     let headerColor = $('header').css('backgroundColor');
-
-    // if vanishingPoint is below aboutTop
-    if (aboutTop < vanishingPoint) {
-        // hide the contents of #hero-wrapper
-        $('#hero-wrapper').css('opacity', 0);
-
-        // make the about section slide up and fade in
-        $('#about-wrapper').animate({
-            opacity: 1,
-            top: 0
-        }, 800);
-    } else {
-        // reveal the #hero-wrapper
-        $('#hero-wrapper').css('opacity', 1);
-    }
 
     function constellationGlow() {
         // play the star-pulse keyframes animation
@@ -109,27 +91,62 @@ $(window).on('scroll', function() {
         }, 2000);
     }
 
-    // if the header is over the...
-    if (headerHeight > projectsTop) {
-        // projects section: make the header bg color blue
-        $('header').css('backgroundColor', 'rgb(121, 141, 175)');
-        // display the default constellation
-        $('#constellation').attr('src', './images/stars/constellation.svg');
-        constellationGlow();
-    } else if (headerHeight > aboutTop) {
-        // about section: make the header bg color olive
-        $('header').css('backgroundColor', 'rgb(106, 120, 86)');
-        // display the yellow constellation
-        $('#constellation').attr('src', './images/stars/yellow-constellation.svg');
-        constellationGlow();
-    } else {
-        // hero section: make the header bg color charcoal 
-        $('header').css('backgroundColor', 'rgb(32, 30, 30)');
-        // display the default constellation
-        $('#constellation').attr('src', './images/stars/constellation.svg');
-        constellationGlow();
-    }
+    // for elements on the index page
+    if ($('#about, #hero, #projects').length) {
+        let aboutTop = $('#about').offset().top - $(window).scrollTop();
+        let vanishingPoint = $('#hero').height() * .75;
+        let projectsTop = Math.floor($('#projects').offset().top) - $(window).scrollTop();
 
+        // if vanishingPoint is below aboutTop
+        if (aboutTop < vanishingPoint) {
+            // hide the contents of #hero-wrapper
+            $('#hero-wrapper').css('opacity', 0);
+
+            // make the about section slide up and fade in
+            $('#about-wrapper').animate({
+                opacity: 1,
+                top: 0
+            }, 800);
+        } else {
+            // reveal the #hero-wrapper
+            $('#hero-wrapper').css('opacity', 1);
+        }
+
+        // if the header is over the...
+        if (headerHeight > projectsTop) {
+            // projects section: make the header bg color blue
+            $('header').css('backgroundColor', 'rgb(121, 141, 175)');
+            // display the default constellation
+            $('#constellation').attr('src', './images/stars/constellation.svg');
+            constellationGlow();
+        } else if (headerHeight > aboutTop) {
+            // about section: make the header bg color olive
+            $('header').css('backgroundColor', 'rgb(106, 120, 86)');
+            // display the yellow constellation
+            $('#constellation').attr('src', './images/stars/yellow-constellation.svg');
+            constellationGlow();
+        } else {
+            // hero section: make the header bg color charcoal 
+            $('header').css('backgroundColor', 'rgb(32, 30, 30)');
+            // display the default constellation
+            $('#constellation').attr('src', './images/stars/constellation.svg');
+            constellationGlow();
+        }
+    } 
+
+    // for elements on the illustration page
+    if ($('#patterns').length) {
+        let patternsTop = Math.floor($('#patterns').offset().top) - $(window).scrollTop();
+
+        if (headerHeight > patternsTop) {
+            // patterns section: make the header bg color jade green
+            $('header').css('backgroundColor', 'rgb(111, 143, 120)');
+            // display the yellow constellation
+            $('#constellation').attr('src', './images/stars/yellow-constellation.svg');
+            constellationGlow();
+        } 
+    }
+    
     // update Safari tab color, as the header color changes
     $('meta[name="theme-color"]').attr('content', headerColor);
 });
