e77447add7fff4cfd33e3880b4f74958a664e950
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-08-20 23:07:26 -0400

    update five petals of venus with src
    Added the src property.

--- js/main.js
+++ js/main.js
@@ -672,6 +672,7 @@ const albumBlurbs = [
     {
         name: 'venus-info',
         title: 'Five Petals of Venus',
+        src: './images/albums/fpov_kairras.png',
         info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio tempore eos earum corrupti tempora deleniti alias. Veritatis eum esse explicabo excepturi neque perferendis quod reprehenderit suscipit quaerat reiciendis, accusamus ad.'
     },
     {
