7b4710d20b9e9e1730c8cea852aceeb3455db77f
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-08-22 21:11:30 -0400

    update SC color
    Switched to a lighter tint.

--- js/main.js
+++ js/main.js
@@ -655,7 +655,7 @@ const albumBlurbs = [
         name: 'cadet-info',
         title: 'Space Cadet',
         src: './images/albums/space-cadet.svg',
-        color: 'rgb(181, 69, 28)',
+        color: 'rgb(216, 157, 136)',
         info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio tempore eos earum corrupti tempora deleniti alias. Veritatis eum esse explicabo excepturi neque perferendis quod reprehenderit suscipit quaerat reiciendis, accusamus ad.'
     },
     {
