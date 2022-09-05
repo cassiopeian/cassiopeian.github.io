e62ee307f291e44116ffb0e751daa8d1f540aba2
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-09-04 21:04:23 -0400

    update GFY src property
    Updated the GFY image in the popup.

--- js/main.js
+++ js/main.js
@@ -686,7 +686,7 @@ const albumBlurbs = [
     {
         name: 'gfy-info',
         title: 'GFY',
-        src: './images/albums/get-bent.svg',
+        src: './images/albums/gfy.svg',
         color: 'rgb(154, 196, 198)',
         info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio tempore eos earum corrupti tempora deleniti alias. Veritatis eum esse explicabo excepturi neque perferendis quod reprehenderit suscipit quaerat reiciendis, accusamus ad.',
         link: 'https://open.spotify.com/playlist/4oDF0zFDuhKs9qbPz1B4ue?si=d04b31542ffa4cb2'
