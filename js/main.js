80f7a7201d172d91cc670d859a1f642dc8016369
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-09-06 20:42:03 -0400

    update albumBlurbs
    Added Rom-com Soundtrack.

--- js/main.js
+++ js/main.js
@@ -690,6 +690,11 @@ const albumBlurbs = [
         color: 'rgb(154, 196, 198)',
         info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio tempore eos earum corrupti tempora deleniti alias. Veritatis eum esse explicabo excepturi neque perferendis quod reprehenderit suscipit quaerat reiciendis, accusamus ad.',
         link: 'https://open.spotify.com/playlist/4oDF0zFDuhKs9qbPz1B4ue?si=d04b31542ffa4cb2'
+    },
+    {
+        name: 'romcom-info',
+        title: 'Rom-com Soundtrack',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio tempore eos earum corrupti tempora deleniti alias. Veritatis eum esse explicabo excepturi neque perferendis quod reprehenderit suscipit quaerat reiciendis, accusamus ad.'
     }
 ];
 
